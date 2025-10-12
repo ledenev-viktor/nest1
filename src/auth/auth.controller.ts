acf3800d7b7a64cd6a79fc1f31c7c232d1ab3610
ledenev <ledenev4252@gmail.com>
2025-10-12 23:14:47 +0300

    start project

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, HttpCode, Post } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+  @Post('register')
+  async register() {}
+
+  @HttpCode(200)
+  @Post('login')
+  async login() {}
+}
---
+++ src/top-page/top-page.model/top-page.model.ts
@@ -0,0 +1,25 @@
+export enum TopLevelCategory {
+  Courses,
+  Services,
+  Books,
+  Products,
+}
+export class TopPageModel {
+  firstCategory: TopLevelCategory;
+  SecondCategory: string;
+  title: string;
+  category: string;
+  hh?: {
+    count: number;
+    juniorSalary: number;
+    middleSalary: number;
+    seniorSalary: number;
+  };
+  advantages: {
+    title: string;
+    description: string;
+  }[];
+  seoText: string;
+  tagsTitle: string;
+  tags: string[];
+}
